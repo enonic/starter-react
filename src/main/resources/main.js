48a1ad2dea0c47f49744d792f7593140d812b547
olaven <olavsundfoer@gmail.com>
2018-07-05 15:43:45 +0200

    working on displaying

--- src/main/resources/main.js
+++ src/main/resources/main.js
@@ -1,6 +1,23 @@
 
-exports.get = function (req) {
+var io = require('/lib/xp/io'); 
+
+var res1 = io.getResource("./index.html");
+var size = res1.getSize();
+var stream = res1.getStream(); 
+
+var text = io.readText(stream); 
+
+exports.get = function(){
     return {
-        body: "hello"
+        body : text
     }
-};
+}
+
+
+/**
+ * Notes: 
+ * 
+ * - get IO to work 
+ * Then, return the text from a HTML-file. 
+ * The HTML-file contains a reference to webpack bundle 
+ */

b029d48e7502b0d1455220ea701b14fe955eee56
olaven <olavsundfoer@gmail.com>
2018-07-05 14:14:30 +0200

    added description

--- src/main/resources/main.js
+++ src/main/resources/main.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -7,7 +8,9 @@ import App from '../app/App';
 const title = React.createElement('h1', {}, 'My First React Code');
 
 ReactDOM.render(<App />, document.getElementById('app'))
-
-exports.get = {
-    body: title
+*/
+exports.get = function () {
+    return {
+        body: 'Hello app'
+    }
 }

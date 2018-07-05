bee7a55e26adfe468b6bb77ae71894ec5c60aa01
EdvardGB <edvard_bakken@live.no>
2018-07-05 14:11:45 +0200

    clean start
    Co-authored-by: olaven <olavsundfoer@gmail.com>

--- src/main/resources/main.js
+++ src/main/resources/main.js
@@ -1,13 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 
-// main app
-import App from '../app/App';
-
-const title = React.createElement('h1', {}, 'My First React Code');
-
-ReactDOM.render(<App />, document.getElementById('app'))
-
-exports.get = {
-    body: title
-}
+exports.get = function (req) {
+    return {
+        body: "hello"
+    }
+};

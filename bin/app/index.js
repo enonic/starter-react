f3d6d7f56d04743b43859b46e14f3b86af6a4f8d
olaven <olavsundfoer@gmail.com>
2018-07-06 09:39:37 +0200

    fixed merge conflict

--- bin/app/index.js
+++ bin/app/index.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-console.log("hello from js")
-
-/**
- * Entry point for webpack 
- */
-=======
->>>>>>> 8b131c380b182f97b4fb212e34963513a8bc2dba
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -13,7 +5,3 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'))
-<<<<<<< HEAD
-=======
-
->>>>>>> 8b131c380b182f97b4fb212e34963513a8bc2dba

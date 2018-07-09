36d41e5ed454fded5f9dd75848cabbce09385a72
olaven <olavsundfoer@gmail.com>
2018-07-09 13:14:27 +0200

    adding dnamic cateories to sidebar

--- src/main/resources/app/containers/sidebar.js
+++ src/main/resources/app/containers/sidebar.js
@@ -3,9 +3,16 @@ import PropTypes from 'prop-types';
 
 // Material UI 
 import Drawer from "@material-ui/core/Drawer";
-import Button from "@material-ui/core/Button";
-import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close'; 
+import Divider from '@material-ui/core/Divider'
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import CategoryIcon from "@material-ui/icons/Store"; 
+
+
 
 
 // Interfaces 
@@ -20,22 +27,55 @@ class SideBar extends React.PureComponent {
 
     constructor(props) {
         super(props)
+        this.state = {
+            listItems : []
+        }
     }
 
     componentDidMount() {
+        // fetch list from redux-store? 
+        this.setState({
+            listItems : [
+                {
+                    title : "Category 1", 
+                    action : "FILTER CATEGORY 1"
+                }, 
+                {
+                    title: "Category 2",
+                    action: "FILTER CATEGORY 2"
+                }
+            ]
+        })
+    }
+
+    renderList() {
+        return this.state.listItems.map((item, index) => {
+            return <ListItem onClick={() => { alert("action dispatch: ", item.action) }}>
+                <ListItemIcon>
+                    <CategoryIcon />
+                </ListItemIcon>
+                <ListItemText>{item.title}</ListItemText>
+            </ListItem>
+        })
     }
 
     render() {
         return (
             <Drawer anchor="left" open={this.props.open}>
-                Categories..
+                <IconButton color="inherit" aria-label="Menu" onClick={this.props.onToggleMenu}>
+                    <CloseIcon />
+                </IconButton>
+                <Divider />
+                <List>
+                    {this.renderList.bind(this)()}                    
+                </List>
             </Drawer>
         );
     }
 }
 
 SideBar.propTypes = {
-    sidebarItems: PropTypes.object // {title : string, action : REDUX-action}
+    onToggleMenu: PropTypes.func,
 };
 
 SideBar.defaultProps = {

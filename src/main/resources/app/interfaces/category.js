c102a88583437d3b1ef8dbd3dcc789d0b56c5b9b
olaven <olavsundfoer@gmail.com>
2018-07-10 14:56:11 +0200

    fixed 'duplicate key'-bug

--- src/main/resources/app/interfaces/category.js
+++ src/main/resources/app/interfaces/category.js
@@ -1,7 +1,7 @@
 export default class Item {
     constructor(data){
         this.title = data.title; 
-        this.id = new Date().valueOf(); 
+        this.id = data.id || new Date().valueOf(); 
         this.visible = false; 
         this.filter = data.filter; 
     }

e4bedd1b162ae65784419566dd7c8ad2cf16c65b
olaven <olavsundfoer@gmail.com>
2018-07-26 13:51:10 +0200

    improved look of 'add to cart'-box

--- src/main/resources/app/components/dialogComponent.js
+++ src/main/resources/app/components/dialogComponent.js
@@ -1,148 +1,156 @@
-import React from 'react';
+    import React from 'react';
 
-import Dialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogActions from '@material-ui/core/DialogActions';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography'; 
+    import Dialog from '@material-ui/core/Dialog';
+    import DialogTitle from '@material-ui/core/DialogTitle';
+    import DialogContent from '@material-ui/core/DialogContent';
+    import DialogActions from '@material-ui/core/DialogActions';
+    import Button from '@material-ui/core/Button';
+    import Typography from '@material-ui/core/Typography'; 
+    import CardMedia from '@material-ui/core/CardMedia'; 
 
-import StorefrontItemComponent from './storefront/storefrontItemComponent';
+    import StorefrontItemComponent from './storefront/storefrontItemComponent';
 
-import CheckoutComponent from './checkoutComponent';
+    import CheckoutComponent from './checkoutComponent';
 
-import CreateItemComponent from './admin/createItemComponent';
-import CreateCategoryComponent from './admin/createCategoryComponent';
-
-
-const styles = theme => ({
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  formControl: {
-    margin: theme.spacing.unit,
-    minWidth: 120,
-  },
-});
+    import CreateItemComponent from './admin/createItemComponent';
+    import CreateCategoryComponent from './admin/createCategoryComponent';
 
 
-export default class DialogComponent extends React.PureComponent { 
-  
-  constructor(props){
-		super(props)
-		this.state = {
-			open: this.props.open
-		};
-	}
+    const styles = theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 120,
+    },
+    });
 
-	componentWillReceiveProps(nextProps){
-    if(nextProps.open !== this.state.open) {
-        this.setState({open:nextProps.open});
+
+    export default class DialogComponent extends React.PureComponent { 
+    
+    constructor(props){
+            super(props)
+            this.state = {
+                open: this.props.open
+            };
+        }
+
+        componentWillReceiveProps(nextProps){
+        if(nextProps.open !== this.state.open) {
+            this.setState({open:nextProps.open});
+        }
     }
-  }
 
-	handleChange = prop => event => {
-    this.setState({ [prop]: event.target.value });
-  };
-
-
-  renderDialog(){
-    switch (this.props.type) {
-      case "ITEM": 
-      case "CATEGORY":
-        return <Dialog disableBackdropClick open={this.state.open} onClose={this.props.onClose}>
-            <DialogContent>{this.getFormType()}</DialogContent>
-          </Dialog>;
-      case "ITEM_VIEW":
-        return (
-          <Dialog
-            open={this.state.open}
-            onClose={this.props.onClose}
-          >
-            <DialogContent>
-              <StorefrontItemComponent item={this.props.item}/>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={this.props.onClose} color="primary">
-                Cancel
-              </Button>
-              <Button onClick={() =>this.props.addToCart(this.props.item)} 
-                color="primary">
-                Add to cart +
-              </Button>
-            </DialogActions>
-          </Dialog>
-        )
-
-      case "CHECKOUT":
-        return (
-          <Dialog
-            open={this.state.open}
-            onClose={this.props.onClose}
-          >
-          <DialogTitle>Choose your payment method</DialogTitle>
-            <DialogContent>
-              <CheckoutComponent onItemsBought={this.props.onItemsBought}/>
-            </DialogContent>
-          </Dialog>
-        )
-      case "DELETE":
-        return (
-          <Dialog
-            open={this.state.open}
-            onClose={this.props.onClose}
-          >
-          <DialogTitle>Do you want to delete this item?</DialogTitle>
-            <DialogContent>
-              <Typography>{this.props.message}</Typography>
-              <Button onClick={this.props.onClose} color="primary">
-                Cancel
-              </Button>
-              <Button onClick={() => {
-                  this.props.remove(this.props.toBeRemoved)
-                  this.props.onClose()
-                }} >
-                Delete
-              </Button>
-            </DialogContent>
-          </Dialog>
-        )
+        handleChange = prop => event => {
+        this.setState({ [prop]: event.target.value });
+    };
 
 
-      default:
-        return null;
+    renderDialog(){
+        switch (this.props.type) {
+        case "ITEM": 
+        case "CATEGORY":
+            return <Dialog disableBackdropClick open={this.state.open} onClose={this.props.onClose}>
+                <DialogContent>{this.getFormType()}</DialogContent>
+            </Dialog>;
+        case "ITEM_VIEW":
+            return (
+            <Dialog
+                open={this.state.open}
+                onClose={this.props.onClose}
+            >
+                <DialogContent>
+                {/*<StorefrontItemComponent item={this.props.item}/>*/}
+                    <Typography align="center" variant="display3" style={{textDecoration : "underline"}}> 
+                        {this.props.item.name}
+                    </Typography>
+                    <CardMedia image={this.props.item.image} style={{height : "500px", width: '500px'}}/>
+                    <Typography variant="display1">
+                        {this.props.item.info}
+                    </Typography>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={this.props.onClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={() =>this.props.addToCart(this.props.item)} 
+                        color="primary">
+                        Add to cart +
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            )
+
+        case "CHECKOUT":
+            return (
+            <Dialog
+                open={this.state.open}
+                onClose={this.props.onClose}
+            >
+            <DialogTitle>Choose your payment method</DialogTitle>
+                <DialogContent>
+                <CheckoutComponent onItemsBought={this.props.onItemsBought}/>
+                </DialogContent>
+            </Dialog>
+            )
+        case "DELETE":
+            return (
+            <Dialog
+                open={this.state.open}
+                onClose={this.props.onClose}
+            >
+            <DialogTitle>Do you want to delete this item?</DialogTitle>
+                <DialogContent>
+                <Typography>{this.props.message}</Typography>
+                <Button onClick={this.props.onClose} color="primary">
+                    Cancel
+                </Button>
+                <Button onClick={() => {
+                    this.props.remove(this.props.toBeRemoved)
+                    this.props.onClose()
+                    }} >
+                    Delete
+                </Button>
+                </DialogContent>
+            </Dialog>
+            )
+
+
+        default:
+            return null;
+        }
     }
-  }
 
-	getFormType(){
-		switch (this.props.type) {
-      case "ITEM":
-        return <CreateItemComponent 
-          submit={this.props.submit} 
-          onClose={this.props.onClose} 
-          categories={this.props.categories}
-          addImage={this.props.addImage} 
-          images={this.props.images}
-          itemToBeEdited={this.props.toBeEdited}
-        />
-      case "CATEGORY":
-        return <CreateCategoryComponent 
-          submit={this.props.submit} 
-          onClose={this.props.onClose}
-          categoryToBeEdited={this.props.toBeEdited}
-        />
-			default:
-				return null;
-		}
-	}
+        getFormType(){
+            switch (this.props.type) {
+        case "ITEM":
+            return <CreateItemComponent 
+            submit={this.props.submit} 
+            onClose={this.props.onClose} 
+            categories={this.props.categories}
+            addImage={this.props.addImage} 
+            images={this.props.images}
+            itemToBeEdited={this.props.toBeEdited}
+            />
+        case "CATEGORY":
+            return <CreateCategoryComponent 
+            submit={this.props.submit} 
+            onClose={this.props.onClose}
+            categoryToBeEdited={this.props.toBeEdited}
+            />
+                default:
+                    return null;
+            }
+        }
 
 
-  render() {
-		return(
-			<div>
-        {this.renderDialog()}
-      </div>
-		)
-	}
-}
+    render() {
+            return(
+                <div>
+            {this.renderDialog()}
+        </div>
+            )
+        }
+    }
